fc68c1fa26085175cf0c1712e93741bf656e0dfa
xtkcx <168980047+xtkcx@users.noreply.github.com>
2026-02-27 22:03:06 +0900

    広告テスト

--- components/RakutenBanner.tsx
+++ components/RakutenBanner.tsx
@@ -4,15 +4,16 @@ export default function RakutenBanner() {
   return (
     <div className="w-full flex justify-center py-6">
       <a
-        href="https://hb.afl.rakuten.co.jp/hsc/516426db.e384d53a.516411f5.48a30454/?link_type=pict&ut=eyJwYWdlIjoic2hvcCIsInR5cGUiOiJwaWN0IiwiY29sIjoxLCJjYXQiOiIyOSIsImJhbiI6MjEwMTYxOCwiYW1wIjpmYWxzZX0%3D"
+        href="https://hb.afl.rakuten.co.jp/hsc/516470d4.8a0b11d6.516411f5.48a30454/?link_type=pict&ut=eyJwYWdlIjoic2hvcCIsInR5cGUiOiJwaWN0IiwiY29sIjoxLCJjYXQiOiIyOSIsImJhbiI6MzI3NjIxNywiYW1wIjpmYWxzZX0%3D"
         target="_blank"
         rel="nofollow sponsored noopener"
         style={{ wordWrap: "break-word" }}
       >
         <img
-          src="https://hbb.afl.rakuten.co.jp/hsb/516426db.e384d53a.516411f5.48a30454/?me_id=1&me_adv_id=2101618&t=pict"
-          alt="楽天バナー広告"
-          style={{ margin: 2 }}
+          src="https://hbb.afl.rakuten.co.jp/hsb/516470d4.8a0b11d6.516411f5.48a30454/?me_id=1&me_adv_id=3276217&t=pict"
+          alt=""
+          title=""
+          style={{ margin: 2, border: "0", display: "block" }}
         />
       </a>
     </div>
